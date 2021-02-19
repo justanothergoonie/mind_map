98392245e390fa498198d82e35ba5cd66fd30cdb
Cody J Powell <codyj.powell@outlook.com>
2021-02-19 14:41:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ session_start();
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <title>Add a Title</title>
+    <title>Mind Brain</title>
     <meta name="description" content="add a description" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
@@ -18,6 +18,31 @@ session_start();
 </head>
 
 <body>
+
+    <form method="post">
+        <input type="hidden" name="_action" value="signup">
+
+
+
+        <label>Whats The Word</label>
+        <input type="text" name="username">
+        <br />
+
+        <label>Font Color</label>
+        <select name="Color">
+            <option value="red">Red</option>
+        </select>
+        <br />
+
+        <label>Font Style</label>
+        <select name="font">
+            <option value="roboto">roboto</option>
+        </select>
+
+        <br />
+
+        <input type="submit" value="Create">
+    </form>
 
 
 </body>
