a8ede6bd8b67e776302bd3055d4c245e898b05cb
Cody J Powell <codyj.powell@outlook.com>
2021-02-22 13:38:42 -0500

    updated db.sql

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Feb 21, 2021 at 06:40 PM
+-- Generation Time: Feb 22, 2021 at 06:37 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
@@ -22,14 +22,14 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `mind_map` (
   `id` int(11) NOT NULL,
-  `name` varchar(19) NOT NULL,
-  `word` varchar(19) NOT NULL,
-  `x` int(5) NOT NULL,
-  `y` int(5) NOT NULL,
-  `size` int(9) NOT NULL,
-  `color` text NOT NULL,
-  `font` text NOT NULL,
-  `rotation` int(3) NOT NULL,
+  `name` varchar(19) DEFAULT NULL,
+  `word` varchar(19) DEFAULT NULL,
+  `x` int(5) DEFAULT NULL,
+  `y` int(5) DEFAULT NULL,
+  `size` int(9) DEFAULT NULL,
+  `color` text,
+  `font` text,
+  `rotation` int(3) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -39,8 +39,15 @@ CREATE TABLE `mind_map` (
 --
 
 INSERT INTO `mind_map` (`id`, `name`, `word`, `x`, `y`, `size`, `color`, `font`, `rotation`, `created_at`, `deleted_at`) VALUES
-(1, 'Cody', 'Pressure', 100, 100, 10, 'Red', 'Roboto', 180, '2021-02-19 19:06:02', NULL),
-(2, 'Cody', 'under', 100, 100, 5000, 'Blue', 'Roboto', 360, '2021-02-21 02:13:45', NULL);
+(21, 'Cody', 'Pressure', NULL, NULL, 3, 'orange', 'netsrak', NULL, '2021-02-22 02:50:27', NULL),
+(25, 'James', 'Bum', 500, 500, 3, 'indigo', 'netsrak', NULL, '2021-02-22 02:55:37', NULL),
+(38, 'Cody', 'Under', NULL, NULL, 1, 'red', 'Billy', NULL, '2021-02-22 03:17:35', NULL),
+(40, 'james', 'bum', 1000, 500, 5, 'red', 'netsrak', NULL, '2021-02-22 03:23:01', NULL),
+(41, 'james', ' ba', 1500, 500, 2, 'blue', 'Billy', NULL, '2021-02-22 03:23:49', NULL),
+(42, 'james', 'ye', 2000, 500, 3, 'green', 'Billy', NULL, '2021-02-22 03:24:26', NULL),
+(46, 'Bowie', ' terror', 250, 250, 3, 'blue', 'netsrak', 180, '2021-02-22 03:36:26', NULL),
+(47, 'james', 'test', 5000, 5000, 2, 'violet', 'Billy', 245, '2021-02-22 04:37:19', NULL),
+(48, 'james', 'test', 5000, 5000, 2, 'orange', 'netsrak', 220, '2021-02-22 04:37:55', NULL);
 
 --
 -- Indexes for dumped tables
@@ -60,4 +67,4 @@ ALTER TABLE `mind_map`
 -- AUTO_INCREMENT for table `mind_map`
 --
 ALTER TABLE `mind_map`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
